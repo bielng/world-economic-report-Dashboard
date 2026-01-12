76ce2fd3c42de82a4cd6f35c6764699c4b8c4b80
bielng <ngunartaban@gmail.com>
2026-01-11 23:36:20 -0600

    can't read data from excel

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib as plt
 
-df = pd.read_csv(R"C:\Users\TAJ\Desktop\world_economic_report\WorldBank.xlsx")
+df = pd.read_excel(R"C:\Users\TAJ\Desktop\world_economic_report\WorldBank.xlsx")
 
+df.head()
